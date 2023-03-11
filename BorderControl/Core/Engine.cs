59b0c40d070e9ea42c354c76c0f4a80b6670e956
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2023-03-11 16:51:59 +0200

    Add files via upload

---
+++ BorderControl/Core/Engine.cs
@@ -0,0 +1,48 @@
+﻿using BorderControl.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BorderControl.Core
+{
+    public class Engine : IEngine
+    {
+        public void Run()
+        {
+            List<IIdentifiable> identities = new List<IIdentifiable>();
+
+            string command = string.Empty;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string[] info = command.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                IIdentifiable identifiable;
+                if (info.Length == 3)
+                {
+                    string citizneName = info[0];
+                    int citizenAge = int.Parse(info[1]);
+                    string citizenId = info[2];
+                    identifiable = new Citizen(citizneName, citizenAge, citizenId);
+
+                    identities.Add(identifiable);
+                }
+                else if (info.Length == 2)
+                {
+                    string robotModel = info[0];
+                    string robotId = info[1];
+                    identifiable = new Robot(robotModel, robotId);
+
+                    identities.Add(identifiable);
+                }
+            }
+
+            string fakeId = Console.ReadLine();
+
+            foreach (IIdentifiable citizen in identities.FindAll(c => c.Id
+            .EndsWith(fakeId)))
+            {
+                Console.WriteLine(citizen.Id);
+            }
+        }
+    }
+}
---
+++ BorderControl/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using BorderControl.Core;
+using BorderControl.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace BorderControl
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            IEngine engine = new Engine();
+            engine.Run();
+        }
+    }
+}
